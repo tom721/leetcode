ed8fc1c54c4c057793c2ce8258ffab816db7c72f
joonyoung_moon <tom721@naver.com>
2021-04-29 14:34:57 +0900

    63_Unique_Paths_II Solution

---
+++ algorithms/go/63_Unique_Paths_II.go
@@ -0,0 +1,36 @@
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+	dp := make([][]int, m) //number of unique path for each point
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	if obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1 { //no path
+		return 0
+	}
+
+	for i := 0; i < m; i++ {
+		if obstacleGrid[i][0] == 1 {
+			break
+		}
+		dp[i][0] = 1
+	}
+	for i := 0; i < n; i++ {
+		if obstacleGrid[0][i] == 1 {
+			break
+		}
+		dp[0][i] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 1 { //obstacle in point
+				dp[i][j] = 0
+				continue
+			}
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
