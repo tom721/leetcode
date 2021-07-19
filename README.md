da2ce9a46ec178aa3d155ac30b3066b3060227a2
joonyoung moon <tom721@naver.com>
2021-07-19 22:25:24 +0900

    Add 15_3Sum Solution

--- README.md
+++ README.md
@@ -45,5 +45,6 @@ LeetCode
 |40|[Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/) | [Python3](./algorithms/python/5_Longest_Palindromic_Substring.py) |Medium|
 |41|[Two Sum](https://leetcode.com/problems/two-sum/) | [Python3](./algorithms/python/1_Two_Sum.py) |Easy|
 |42|[Trapping Rain Water](https://leetcode.com/problems/trapping-rain-water/) | [Python3](./algorithms/python/42_Trapping_Rain_Water.py) |Hard|
+|43|[3Sum](https://leetcode.com/problems/3sum/) | [Python3](./algorithms/python/15_3Sum.py) |Medium|
 
 This repository references [jwkim1993's leetcode](https://github.com/jwkim1993/leetcode)
---
+++ algorithms/python/15_3Sum.py
@@ -0,0 +1,24 @@
+import itertools
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left, right = i+1, len(nums)-1
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right]
+                if three_sum == 0:
+                    answer.append([nums[i],nums[left],nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif three_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return answer
